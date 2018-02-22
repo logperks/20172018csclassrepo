b7e71af885784754dcb5bd42cdf4a8421fc2f4e3
logperks <32135416+logperks@users.noreply.github.com>
2018-02-22 13:58:53 -0700

    Update script.js

--- Projects/Project3/script.js
+++ Projects/Project3/script.js
@@ -1,30 +1,29 @@
 document.getElementById("gamestart").onclick = function(){
-var correct = Math.floor(Math.random() * 5);
-var attempts = 1;
+  var correct = Math.floor(Math.random() * 5);
+  var attempts = 1;
 
-var game = 1;
-while (game == 1){
-  var guess = prompt("Guess a number between 0 and 5:"); //clicking cancel here counts as an attempt
+  var game = 1;
+  while (game == 1){
+    var guess = prompt("Guess a number between 0 and 5:"); //clicking cancel here counts as an attempt
 
-  if (guess == correct){
-    game = 0;
+    if (guess == correct){
+      game = 0;
+
+
+    } 
+    // end of if statement
+
+    if (guess != correct){
+      attempts = attempts + 1;
+
+
+    } 
+    // end of if statement
 
 
   } 
-  // end of if statement
-
-  if (guess != correct){
-    attempts = attempts + 1;
-
-
-  } 
-  // end of if statement
-
-
-} 
-//end of while statement
-document.getElementById("content").innerHTML = "Correct! Attempts taken: " + attempts;
-
+  //end of while statement
+  document.getElementById("content").innerHTML = "Correct! Attempts taken: " + attempts;
 
 } 
 //end of button function
